da23e605073a9be6330c96e7b093e412d88ae194
Yash Kumar <yashyk9891@gmail.com>
2026-01-20 07:57:12 +0530

    Updated bookingStatus

--- src/main/java/com/yash/project/HotelManagementApp/enums/BookingStatus.java
+++ src/main/java/com/yash/project/HotelManagementApp/enums/BookingStatus.java
@@ -2,6 +2,9 @@ package com.yash.project.HotelManagementApp.enums;
 
 public enum BookingStatus {
     RESERVED,
+    GUESTS_ADDED,
+    PAYMENTS_PENDING,
     CONFIRMED,
-    CANCELLLED
+    CANCELLED,
+    EXPIRED
 }
